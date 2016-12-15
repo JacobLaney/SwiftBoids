986fd48dd0a472688004d063acf607f48f0b1728
Jacob Laney <jake.laney96@gmail.com>
2016-12-15 15:32:20 -0600

    Initial Commit

---
+++ Birds/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Birds
+//
+//  Created by Jake Laney on 12/15/16.
+//  Copyright © 2016 Jake Laney. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
