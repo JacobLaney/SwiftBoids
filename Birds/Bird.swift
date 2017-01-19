716b09ebd2c77ba0d26dc9d6479ac574a7756735
Jacob Laney <jake.laney96@gmail.com>
2017-01-18 23:31:42 -0600

    cleaning up

--- Birds/Bird.swift
+++ Birds/Bird.swift
@@ -29,8 +29,23 @@ class Bird: SKShapeNode {
     static var REPULSION : CGFloat = 100 // scalar affecting repulsion from other objects
     static var CLOSENESS : CGFloat = 1 // scalar affecting attraction to each other
     
+    
     override init() {
         super.init()
+        self.path = CGPath.init(ellipseIn: CGRect.init(x: 0, y: 0, width: 10, height: 10), transform: nil);
+        
+        let scale : CGFloat = self.frame.size.width * CGFloat(1.3 / 2)
+        let field = SKShapeNode(circleOfRadius: scale)
+        field.lineWidth = 0.0
+        field.name = "/bird/field"
+        self.addChild(field) // uncomment for visible field
+        
+        let avoidanceField = SKShapeNode(circleOfRadius: 1.5 * self.frame.size.width)
+        avoidanceField.strokeColor = SKColor.red
+        avoidanceField.lineWidth = 2.0
+        // self.addChild(avoidanceField) // uncomment to add a visual avoidance field to the birds
+        
+        self.fillColor = NSColor.black;
     }
     
     required init?(coder decoder: NSCoder) {
@@ -38,8 +53,21 @@ class Bird: SKShapeNode {
     }
     
     func startMotion() {
+        // add physics body and speed
+        self.physicsBody = SKPhysicsBody.init(rectangleOf: self.frame.size)
+        self.physicsBody?.affectedByGravity = false;
+        self.physicsBody?.linearDamping = 0;
+        self.physicsBody?.restitution = 1
+        self.physicsBody?.friction = 0;
+        self.physicsBody?.density = 1000;
+        self.physicsBody?.angularDamping = 0;
+        self.physicsBody?.contactTestBitMask = self.physicsBody!.collisionBitMask
+        
+        self.name = "/bird";
+        
         let unit_vector = unitify(CGVector.init(dx: getRandomFloat(), dy: getRandomFloat()))
         let vector = scale(unit_vector, Bird.SPEED)
+        
         self.physicsBody?.velocity = vector
     }
     
@@ -95,8 +123,8 @@ class Bird: SKShapeNode {
                     if child is Bird {
                         neighbors.append(child as! Bird)
                         
-                        /*
                         // apply repulsion
+                        /*************************
                         if distance > pow(self.frame.width * 3.0 / 2, 2) {
                             let diff = differenceVector(from: self.position, to: child.position)
                             self.physicsBody?.velocity.add(scale(unitify(diff), Bird.CLOSENESS))
@@ -106,35 +134,32 @@ class Bird: SKShapeNode {
                             self.physicsBody?.velocity.add(scale(unitify(diff), Bird.REPULSION))
                         }
 
-                        if let other = child.childNode(withName: "field") {
-                            if self.childNode(withName: "field")!.intersects(other) {
+                        if let other = child.childNode(withName: "/bird/field") {
+                            if self.childNode(withName: "/bird/field")!.intersects(other) {
                                 let diff = differenceVector(from: child.position, to: self.position)
-                                self.physicsBody?.velocity.add(scale(unitify(diff), REPULSION))
+                                self.physicsBody?.velocity.add(scale(unitify(diff), Bird.REPULSION))
                             }
                         }
-                        */
+                        ***************************/
                     }
                 }
-                
-                
-            
-                
             }
          }
+        
         // move to average position
-         // apply cohesion
+        // apply cohesion
         var xSum : CGFloat = 0
         var ySum : CGFloat = 0
         var dxSum : CGFloat = 0
         var dySum : CGFloat = 0
-        let count : CGFloat = CGFloat(neighbors.count)
+        
         for bird in neighbors {
             xSum += bird.position.x
             ySum += bird.position.y
             dxSum += bird.physicsBody!.velocity.dx;
             dySum += bird.physicsBody!.velocity.dy;
         }
-        let point = CGPoint(x: xSum / count , y: ySum / count)
+        
         let velocity = CGVector.init(dx: dxSum / CGFloat(neighbors.count), dy: dySum / CGFloat(neighbors.count))
         self.physicsBody!.velocity.add(scale(unitify(velocity), Bird.COHESION))
         //self.physicsBody!.velocity.add(scale(unitify(point)), 1)
--- Birds/GameScene.swift
+++ Birds/GameScene.swift
@@ -14,14 +14,40 @@ import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    private var bird : Bird?
-    private var start : TimeInterval!
-    
-    let updateInterval : TimeInterval = 0.0
+    private var bird : Bird! // model for a bird
     
     private var toggled : Bool!
+    
+    override func didMove(to view: SKView) {
+        toggled = false;
+        
+        // add a PAUSE button
+        let toggle = NSButton.init(checkboxWithTitle: "Cohesion", target: self, action: #selector(togglePressed))
+        toggle.state = 1
+        toggle.frame = NSRect.init(x: 10, y: 10, width: 100, height: 10)
+        self.view?.addSubview(toggle)
+        
+        // add a SPEED slider
+        let slider = NSSlider(value: 250, minValue: 1, maxValue: 600, target: self, action: #selector(setSpeed))
+        slider.frame = NSRect.init(x: 10, y: 50, width: 100, height: 30)
+        Bird.SPEED = CGFloat(slider.floatValue)
+        self.view?.addSubview(slider)
+        
+        self.physicsWorld.contactDelegate = self
+        
+        // #### uncomment for frame ####
+        self.physicsBody = SKPhysicsBody.init(edgeLoopFrom: self.frame)
+        self.physicsBody?.isDynamic = false
+        self.physicsBody?.restitution = 1
+        self.physicsBody?.friction = 0
+
+        // Create Bird node to use during mouse interaction
+        let w : CGFloat = (self.size.width) * 0.01
+        print("diameter of circle:  \(w)")
+        self.bird = Bird()
+        
+    }
+    
     func togglePressed(sender: AnyObject) {
         print("PRESSED")
         toggled = !toggled;
@@ -39,64 +65,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
-    override func didMove(to view: SKView) {
-        toggled = false;
-        let toggle = NSButton.init(checkboxWithTitle: "Cohesion", target: self, action: #selector(togglePressed))
-        toggle.state = 1
-        toggle.frame = NSRect.init(x: 10, y: 10, width: 100, height: 10)
-        self.view?.addSubview(toggle)
-        
-        let slider = NSSlider(value: 250, minValue: 1, maxValue: 600, target: self, action: #selector(setSpeed))
-        slider.frame = NSRect.init(x: 10, y: 50, width: 100, height: 30)
-        Bird.SPEED = CGFloat(slider.floatValue)
-        self.view?.addSubview(slider)
-        
-        start = 0
-        
-        self.physicsWorld.contactDelegate = self
-        
-        //self.physicsBody = SKPhysicsBody.init(edgeLoopFrom: self.frame)
-        //self.physicsBody?.isDynamic = false
-        //self.physicsBody?.restitution = 1
-        //self.physicsBody?.friction = 0
-
-        // Create shape node to use during mouse interaction
-        let w : CGFloat = (self.size.width) * 0.01
-        print("diameter of circle:  \(w)")
-        
-        self.bird = Bird.init(ellipseOf: CGSize.init(width: w, height: w))
-        
-        if let bird = self.bird {
-            self.bird?.physicsBody = SKPhysicsBody.init(rectangleOf: self.bird!.frame.size)
-            self.bird?.physicsBody?.affectedByGravity = false;
-            self.bird?.physicsBody?.linearDamping = 0;
-            self.bird?.physicsBody?.restitution = 1
-            self.bird?.physicsBody?.friction = 0;
-            self.bird?.physicsBody?.density = 1000;
-            self.bird?.physicsBody?.angularDamping = 0;
-            self.bird?.physicsBody?.contactTestBitMask = self.physicsBody!.collisionBitMask
-            
-            bird.lineWidth = 3.0;
-            bird.strokeColor = SKColor.black;
-            bird.fillColor = SKColor.black;
-            bird.name = "/bird";
-            
-            let avoidanceField = SKShapeNode(circleOfRadius: 1.5 * w)
-            avoidanceField.strokeColor = SKColor.red
-            avoidanceField.lineWidth = 2.0
-            self.bird?.addChild(avoidanceField)
-
-            let scale : CGFloat = bird.frame.size.width * CGFloat(1.3 / 2)
-            let field = SKShapeNode(circleOfRadius: scale)
-            field.lineWidth = 0.0
-            //field.strokeColor = SKColor.blue
-            field.name = "field"
-            bird.addChild(field)
-        }
-    }
-    
-    //SKAction.run( {bird.check(neighbors: self.birds!)} ),
-    //bird.adjust()
     func touchDown(atPoint pos : CGPoint) {
         if let n = self.bird?.copy() as! Bird? {
             n.position = pos
@@ -125,24 +93,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.touchUp(atPoint: event.location(in: self))
     }
     
-    override func keyDown(with event: NSEvent) {
-        switch event.keyCode {
-        case 0x31:
-            if let label = self.label {
-                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-            }
-        default:
-            print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
-        }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // only update every n seconds
+        /********** UNCOMMENT IF NO WORLD FRAME *******
         let left : CGFloat = -1 * self.frame.width / 2
         let right : CGFloat = self.frame.width / 2
         let up : CGFloat = self.frame.height / 2
         let down : CGFloat = -1 * self.frame.height / 2
+        
             for child in self.children {
                 if let c = child as? Bird {
                     c.update()
@@ -160,52 +118,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                     }
                 }
             }
-    }
-    
-    let shockWaveAction: SKAction = {
-        let growAndFadeAction = SKAction.group([SKAction.scale(to: 50, duration: 0.5),
-                                                SKAction.fadeOut(withDuration: 0.5)])
-        
-        let sequence = SKAction.sequence([growAndFadeAction,
-                                          SKAction.removeFromParent()])
-        
-        return sequence
-    }()
+        ******************************************/
 
-    
-    func didBegin(_ contact: SKPhysicsContact) {
-        /*
-        if  contact.bodyA.node! is Bird {
-            contact.bodyA.velocity = CGVector.init(
-                dx: contact.bodyA.velocity.dx * -1, dy: contact.bodyA.velocity.dy * -1
-            )
+        for child in self.children {
+            if let c = child as? Bird {
+                c.update()
+            }
         }
-        if contact.bodyB.node! is Bird {
-            contact.bodyB.velocity = CGVector.init(
-                dx: contact.bodyB.velocity.dx * -1, dy: contact.bodyB.velocity.dy * -1
-            )
-        }
-        
-            let shockwave = SKShapeNode(circleOfRadius: 1)
-            
-            shockwave.position = contact.contactPoint
-            self.addChild(shockwave)
-            
-            shockwave.run(shockWaveAction)
-            
-            let a = contact.bodyA.node! as! Bird
-            let b = contact.bodyB.node! as! Bird
-            
-            let unitVectorA = a.unitify(a.difference(a.position, b.position))
-            let unitVectorB = b.unitify(b.difference(b.position, a.position))
-            let vectorA = a.scale(unitVectorA, 100)
-            let vectorB = b.scale(unitVectorB, 100)
-            contact.bodyA.applyImpulse(vectorA)
-            contact.bodyB.applyImpulse(vectorB)
-        }
-        */
     }
     
+
+    // contact delegate method
+    func didBegin(_ contact: SKPhysicsContact) {
+
+    }
+
+    // contact delegate method
     func didEnd(_ contact: SKPhysicsContact) {
+        
     }
 }
